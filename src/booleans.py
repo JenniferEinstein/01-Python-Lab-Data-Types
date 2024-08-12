ba6320cc9be702781f82829ca3dc72b54ff52399
JenniferEinstein <jennifereinstein@gmail.com>
2024-08-12 19:45:46 -0400

    Booleans done

--- src/booleans.py
+++ src/booleans.py
@@ -2,8 +2,15 @@
 # -----------------------------------
 # Evaluate the following boolean expressions:
 # 1. (10 > 5) and (5 <= 3) or (100 == 100)
+        # True and False or True
+print(True)
+
 # 2. not (7 != 7) or ((3 < 2) and (4 == 4))
+        # True or False and True
+print(True)
 # 3. (4 ** 2 > 15) and ((100 / 10) < 9) or (5 == 5 and not (10 == 10))
+        # True and True or True and False
+print(True)
 
 # Write the answers in comments below each expression and print the results.
 
@@ -12,7 +19,11 @@
 # Write a Python function that takes two numbers as input and returns True if the first number is divisible by the second, and False otherwise.
 
 def is_divisible(x, y):
-    # Your code here
+    if x % y == 0:
+        return True
+    else: 
+        return False
+
     pass
 
 # Task 3: Complex Logical Operations
@@ -22,7 +33,8 @@ def is_divisible(x, y):
 # 2. Negative, odd, and greater than -10.
 
 def complex_check(n):
-    # Your code here
+    if (n>0 and n%2==0 and n%3==0) or (n<0 and n%2==1 and n>-10):
+        return True
     pass
 
 # Task 4: Boolean Type Conversion
@@ -36,4 +48,19 @@ def complex_check(n):
 # 6. []
 
 # Use the `bool()` function and print the results with comments explaining the outcomes.
+
+print(bool(0))
+    # 0 is a falsy value
+print(bool(3.14))
+    # fractions have a truthy value
+print(bool(-100))
+    # integers have a truthy value 
+print(bool(""))
+    # Empty objects, including strings, are always False
+print(bool("False"))
+    # A string with anything in it -- even the word False -- is a truthy value. 
+print(bool([]))
+    # Empty objects are always False
+
+
 
